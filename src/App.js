392bb0928d70b5cd957cb435a1225c1f46f72df2
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-06-11 09:41:23 -0400

    Import all files and render them on the screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./App.css";
+import Counter from "./counter";
+import NameSetter from "./name";
+import ToggleVisibility from "./visibility";
+import MirrorText from "./textMirror";
+import UppercaseConverter from "./uppercase";
+import DropdownBar from "./dropdown";
+import PasswordConfirmation from "./password";
+import CharCount from "./inputCount";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useState() Practice:</h1>
+      <h2>Name Generator:</h2>
+      <NameSetter />
+      <br />
+      <h2>Number Counter:</h2>
+      <Counter />
+      <br />
+      <h2>Toggle Visibility:</h2>
+      <ToggleVisibility />
+      <br />
+      <h2>Text Mirror:</h2>
+      <MirrorText />
+      <br />
+      <h2>Uppercase Converter:</h2>
+      <UppercaseConverter />
+      <br />
+      <h2>Option Selector:</h2>
+      <DropdownBar />
+      <br />
+      <h2>Password Confirmer:</h2>
+      <PasswordConfirmation />
+      <br />
+      <h2>Character count</h2>
+      <CharCount />
     </div>
   );
 }
