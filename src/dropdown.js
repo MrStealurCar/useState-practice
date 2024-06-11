88a490b13ab893362ed1f5aab4395494cb5aeeff
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-06-11 09:34:26 -0400

    Add pizza topping selector

---
+++ src/dropdown.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+
+export default function DropdownBar() {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleEvent = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Select a Pizza Topping</h2>
+      <select value={selectedValue} onChange={handleEvent}>
+        <option value="">Select a pizza topping</option>
+        <option value="Pepperoni">Pepperoni</option>
+        <option value="Onions">Onions</option>
+        <option value="Green Peppers">Green Peppers</option>
+      </select>
+      {selectedValue ? <h4>You have selected: {selectedValue}</h4> : null}
+    </div>
+  );
+}
